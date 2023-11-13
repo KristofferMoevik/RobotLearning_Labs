555e867156e410325892be381df4b7c85623b835
KristofferMoevik <kristoffersmovik@live.no>
2023-11-13 16:57:59 +0100

    restructured

--- src/catkin_ws/build/lab01_pkg/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "lab01_pkg"
-PROJECT_SPACE_DIR = "/home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
